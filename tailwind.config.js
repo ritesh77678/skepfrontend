ba39eafd17a7af5fa4ecde966b1ec937467d915e
ritesh77678 <ritesh.gupta@rezolut.in>
2025-12-17 17:23:43 +0530

    frontend setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'primary-light': 'var(--primary-light)',
+        'background-secondary': 'var(--primary-light)', // if you want this name
+      },
+    },
+  },
+};
